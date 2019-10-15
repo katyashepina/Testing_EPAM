16ba4e32705d94302ecb89f3c5b313dc4e1581a7
katyashepina <41111350+katyashepina@users.noreply.github.com>
2019-10-15 15:57:36 +0300

    add file with clean code
    add file with clean code

---
+++ Clean Code/MilitaryPlane.cs
@@ -0,0 +1,44 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        //1
+        public MilitaryType militaryType;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType militaryType)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            //2
+            this.militaryType = militaryType;
+        }
+
+        public override bool Equals(object objectToCompare)
+        {
+            //3
+            return (objectToCompare as MilitaryPlane) != null &&
+                   base.Equals(objectToCompare) &&
+                   militaryType == (objectToCompare as MilitaryPlane).militaryType;
+        }
+
+        public override int GetHashCode()
+        {
+            //4
+            var hashCode = 1701194404;
+            return (hashCode * -1521134295 + base.GetHashCode())
+                * hashCode * -1521134295 + militaryType.GetHashCode();
+        }
+
+        public MilitaryType GetPlaneTypeIs()
+        {
+            //5
+            return militaryType;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",", type=" + militaryType +'}');
+        }        
+    }
+}
---
+++ Clean Code/PassengerPlane.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aircompany.Planes
+{
+    public class PassengerPlane : Plane
+    {
+        //6
+        public int passengersCapacity;
+
+        public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
+            :base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            //7
+            this.passengersCapacity = passengersCapacity;
+        }
+
+        public override bool Equals(object objectToCompare)
+        {
+            //8
+            return (objectToCompare as PassengerPlane) != null &&
+                   base.Equals(objectToCompare) &&
+                   passengersCapacity == (objectToCompare as PassengerPlane).passengersCapacity;
+        }
+
+        public override int GetHashCode()
+        {
+            //9
+            var hashCode = 751774561;
+            return (hashCode * -1521134295 + base.GetHashCode())
+                * hashCode * -1521134295 + passengersCapacity.GetHashCode();
+        }
+
+        public int GetPassengersCapacityIs()
+        {
+            return passengersCapacity;
+        }
+
+       
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",", passengersCapacity=" + passengersCapacity + '}');
+        }       
+    }
+}
