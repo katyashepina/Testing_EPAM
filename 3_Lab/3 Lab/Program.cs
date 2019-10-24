53eee914d9109f275ca8be10a1fd9e93ef3b1c33
Katya Shepina <katya.shepina@list.ru>
2019-10-24 15:41:56 +0300

    add unit tests as 3 lab

---
+++ 3_Lab/3 Lab/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace _3Lab.Tests
+{
+    [TestFixture]
+    public class CreateTriangleTests
+    {
+        [TestCase(-5, 10, 10)]
+        [TestCase(5, -10, 10)]
+        [TestCase(5, 10, -10)]
+        public void NotNegativeInput(int x, int y, int z)
+        {
+            Assert.IsFalse(Triangle.IsTriangle(x, y, z), "Error in NotNegativeInput");
+        }
+        [TestCase(5, 5, 10)]
+        [TestCase(5, 10, 5)]
+        [TestCase(10, 5, 5)]
+        public void NotLine(int x, int y, int z)
+        {
+            Assert.IsFalse(Triangle.IsTriangle(x, y, z), "Error in NotLine 1");
+        }
+        [TestCase(5, 5, 5)]
+        [TestCase(5, 6, 7)]
+        [TestCase(5, 10, 10)]
+        public void CorrectEntries(int x, int y, int z)
+        {
+            Assert.IsTrue(Triangle.IsTriangle(x, y, z), "Error in CorrectEntries");
+        }
+        [TestCase(0, 5, 5)]
+        [TestCase(5, 0, 5)]
+        [TestCase(5, 5, 0)]
+        public void ZeroInput(int x, int y, int z)
+        {
+            Assert.IsFalse(Triangle.IsTriangle(x, y, z), "Error in ZeroInput X");
+        }
+        [TestCase(10, 1, 1)]
+        [TestCase(1, 10, 1)]
+        [TestCase(1, 1, 10)]
+        public void AreEnoughLineLengths(int x, int y, int z)
+        {
+            Assert.IsFalse(Triangle.IsTriangle(x, y, z), "Error in AreEnoughLineLengths 1");
+        }
+    }
+}
+
+namespace _3Lab
+{
+    public static class Triangle
+    {
+        public static bool IsTriangle(double x, double y, double z)
+        {
+            return ((x + y) > z) && ((x + z) > y) && ((z + y) > x);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
