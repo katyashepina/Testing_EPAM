e8a1cb41a79fa26df0931ae628f2d6edc1a2fadd
Katya Shepina <katya.shepina@list.ru>
2019-12-01 19:31:58 +0300

    add webdriver task

---
+++ SeleniumWebDriver_AutoMaxiTest/AutoMaxiTest/AutoMaxiTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriver
+{
+    [TestClass]
+    public class AutoMaxiTest
+    {
+        IWebDriver driver= new ChromeDriver();
+        [TestMethod]
+        public void СallBackTheWrongNameAndPhone()
+        {
+            driver.Navigate().GoToUrl(@"https://www.avtomaxi.ru/");
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.avtomaxi.ru/");
+            driver.FindElement(By.XPath("//span[@class='callback open-popup-link']")).Click();
+            driver.FindElement(By.XPath("//input[@name = 'name']")).SendKeys("111");
+            driver.FindElement(By.XPath("//input[@name = 'phone']")).SendKeys("123");
+            driver.FindElement(By.XPath("//input[@class='blue-btn']")).Click();
+            IWebElement error = GetElement(driver, "//div[@class='success_message']");
+            Assert.AreEqual("Данные указаны неверно.", error.Text);
+            driver.Quit();
+        }
+        [TestMethod]
+        public void PaymentOnlineIncorrectDetails()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.avtomaxi.ru/");
+            driver.FindElement(By.XPath("//span[@class='callback open-popup-link']")).Click();
+            driver.FindElement(By.XPath("//input[@name = 'tenant-name']")).SendKeys("artem");
+            driver.FindElement(By.XPath("//input[@name = 'name']")).SendKeys("katya");
+            driver.FindElement(By.XPath("//input[@name = 'phone']")).SendKeys("katya");
+            driver.FindElement(By.XPath("//input[@name = 'email']")).SendKeys("katya@mail.ru");
+            driver.FindElement(By.XPath("//input[@name = 'price']")).SendKeys("123");
+            driver.FindElement(By.XPath("//input[@name = 'message']")).SendKeys("123");
+            driver.FindElement(By.XPath("//input[@class='green-btn']")).Click();
+            IWebElement error = GetElement(driver, "//h1[@class='title_page']");
+            Assert.AreEqual("Оплата отменена.", error.Text);
+            driver.Quit();
+        }
+        IWebElement GetElement(IWebDriver driver, string xPath)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until<IWebElement>((d) =>
+            {
+                IWebElement webElement = d.FindElement(By.XPath(xPath));
+                if (webElement.Displayed)
+                    return webElement;
+                return null;
+            });
+            return element;
+        }
+    }
+}
---
+++ SeleniumWebDriver_AutoMaxiTest/AutoMaxiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZhdTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZhdTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("950b0a80-58ac-44bf-8047-d1a77da90cd9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
